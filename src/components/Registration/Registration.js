ce2f62e8ed322d7a57839e8acde9f56b70bc98c8
showaibbinnasir <showaibnishad@gmail.com>
2023-03-24 04:29:10 -0500

    toggle login and registration added

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -5,14 +5,20 @@ const Registration = () => {
     return (
         <div className='w-[100vw] h-[100vh] bg-gradient-to-r from-[#3F55A5] to-[#A3519F] flex justify-center items-center'>
             <div className='my-5 lg:my-0 md:my-0'>
-                <h1 className='text-white text-4xl font-bold text-center my-5'>Login</h1>
+                <h1 className='text-white text-4xl font-bold text-center my-5'>Registration</h1>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Username</span>
+                            </label>
+                            <input type="text" placeholder="username" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -27,7 +33,7 @@ const Registration = () => {
                             <button className="btn bg-gradient-to-r from-[#3F55A5] to-[#A3519F] text-white">Login</button>
                         </div>
                     </div>
-                    <h1 className='mx-2 text-center'>Don't have any account? <span className='text-[#A3519F] font-semibold'><Link to='/register'>Register Now!</Link></span></h1>
+                    <h1 className='mx-2 text-center'>Already have account? <span className='text-[#A3519F] font-semibold'><Link to='/login'>Login Now!</Link></span></h1>
                     <h1 className='text-center mb-2'>Or <span className='text-[#A3519F] font-semibold'><Link to='/home'>Go to home</Link></span></h1>
                     <div className='mt-2'>
                         <h1 className='text-center'>Or Login with</h1>
